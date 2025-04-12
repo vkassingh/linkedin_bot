62b3deaffd0f1f9fab54ed98721f014328135830
Kavita wani <113329083+kavita346@users.noreply.github.com>
2025-04-12 19:22:11 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -10,5 +10,10 @@ router.post('/', postController.createPost);
 // Delete a post
 router.delete('/:postId', postController.deletePost);
 
+// Get all posts
+router.get('/', postController.getAllPosts);
+
+// Get a single post by ID
+router.get('/:id', postController.getPostById);
 
 module.exports = router;
